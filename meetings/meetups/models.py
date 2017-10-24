98ba1df3e1817063516e3882ceaa36657fcb6a89
Sergiy Sych <sychsergiy@gmail.com>
2017-10-24 20:32:33 +0300

    Event and EventUser models added

--- meetings/meetups/models.py
+++ meetings/meetups/models.py
@@ -18,8 +18,21 @@ PERIOD_CHOICES = (
 
 class Rule(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
 
     period = models.CharField(max_length=120, choices=PERIOD_CHOICES)
     category = models.CharField(max_length=120, choices=CATEGORY_CHOICES)
+
+
+class Event(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    place = models.CharField(max_length=120)
+    description = models.TextField()
+
+
+class EventUser(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    event = models.ForeignKey(Event)
+    answered = models.BooleanField(default=False)
+    accepted = models.NullBooleanField()
